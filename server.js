6ab3cba1fc730c09eef5674ea29b603eab06d589
Shamila <shamila.m09@gmail.com>
2019-03-12 18:47:19 -0700

    almost done

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, './client')));
 
-var routeSetter = require('./server/config/routes.js');
-routeSetter(app);
-
-// app.listen(8000, function() {
-// 	console.log('The server is listening on port 8000');
-// });
 
 app.listen(process.env.PORT || 8000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
