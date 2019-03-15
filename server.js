3be3eb82882d3381fe53f4d2ebb1ef161e104fa0
Shamila <shamila.m09@gmail.com>
2019-03-14 17:33:00 -0700

    progress

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var path = require('path');
 var app = express();
 
+// app.use(express.static(path.join(__dirname, './client')));
 app.use(express.static(path.join(__dirname, './client')));
-
 
 app.listen(process.env.PORT || 8000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
